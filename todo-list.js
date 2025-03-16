a505cf72d6684ebb26710bdfae6535b0a8f9616c
SRIKANTH-0001 <2020tamilarasi115@gmail.com>
2025-03-16 11:54:41 +0530

    Your Files are Added

---
+++ todo-list.js
@@ -0,0 +1,11 @@
+const todoList=[];
+
+function addtodo(){
+   const inputElement = document.querySelector('.input');
+   const name=inputElement.value;
+   todoList.push(name);
+   console.log(todoList);
+
+   inputElement.value='';
+}
+
